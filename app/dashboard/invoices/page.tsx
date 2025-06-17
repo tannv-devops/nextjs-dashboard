a8724256ef971e1d9604e31c337f724a50a0b146
Tan Nguyen Van <tannv.devops@gmail.com>
2025-06-17 14:34:15 +0700

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Page() {
+    return <p>Invoices Page</p>;
+
+}
+// This is a placeholder for the Invoices page.
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Page() {
+    return <p>Dashboard Page</p>;
+
+}
+// This is a placeholder for the dashboard page.
