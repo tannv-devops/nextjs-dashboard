0364e43b230a24501594ac725bdc9b99d898e109
Tan Nguyen Van <tannv.devops@gmail.com>
2025-06-30 12:37:50 +0700

    update sql

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,31 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
+import {cookies} from 'next/headers';
+import { NextRequest } from 'next/server';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+async function listInvoices() {
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
 
-// 	return data;
-// }
+	return data;
+}
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+export async function GET(rewquest: NextRequest) {
+
+  const cookieStore = await cookies();
+
+  const a = cookieStore.get('a');
+  const b = cookieStore.set('b', '123');
+  console.log('Cookie b:', b);
+
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
 }
